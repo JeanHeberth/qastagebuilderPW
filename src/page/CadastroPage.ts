33dc13b0bb5799098026b6dd136f20e45041cd1e
jeanheberth <jeanheberth19@gmail.com>
2024-10-09 13:53:15 -0300

    corrigindo relatorio

--- src/page/CadastroPage.ts
+++ src/page/CadastroPage.ts
@@ -45,12 +45,48 @@ export class CadastroPage extends CadastroElements {
         await this.page.locator(CadastroElements.DROPDOWN_NIVEL_PROFICIENCIA(nivelProficiencia)).click()
     }
 
-    async clicarNosTermosDeUso(){
+    async clicarNosTermosDeUso() {
         await this.page.locator(CadastroElements.TERMO_DE_USO).click()
     }
 
-    async clicarNoBotaoProximo(){
+    async clicarNoBotaoProximo() {
         await this.page.locator(CadastroElements.BTN_PROXIMO).click()
     }
+
+    // Inscrição de endereco
+
+    async digitarCep(cep: string) {
+        await this.page.locator(CadastroElements.INPUT_CEP).fill(cep);
+    }
+
+    async digitarCasa(casa: string) {
+        await this.page.locator(CadastroElements.INPUT_CASA).fill(casa);
+    }
+
+    async digitarComplemento(complemento: string) {
+        await this.page.locator(CadastroElements.INPUT_COMPLEMENTO).fill(complemento);
+    }
+
+    async clicarNoBotaoProximoTelaEndereco() {
+        await this.page.waitForLoadState('networkidle');
+        this.page.locator(CadastroElements.BOTAO_PROXIMO_TELA_ENDERECO).click();
+    }
+
+    async validarUrl() {
+        await this.page.locator(CadastroElements.URL)
+    }
+
+    async clicarBotaoAcessarPlataforma() {
+        await this.page.locator(CadastroElements.BTN_ACESSAR_PLATAFORMA).click()
+    }
+
+    async realizarLogin(login: string, password: string) {
+        await this.page.locator(CadastroElements.LOGIN).fill(login);
+        await this.page.locator(CadastroElements.LOGIN_PASSWORD).fill(password);
+        await this.page.locator(CadastroElements.BTN_ENTRAR).click()
+
+    }
+
+// Espera até que o botão esteja visível
 
 }
